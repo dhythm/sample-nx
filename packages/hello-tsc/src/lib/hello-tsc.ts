8ab37ed77ea186d26d7b37c0037d49ad4ba02551
dhythm <yuta.okada.20@gmail.com>
2022-08-13 22:16:53 +0900

    work with default import for lodash

--- packages/hello-tsc/src/lib/hello-tsc.ts
+++ packages/hello-tsc/src/lib/hello-tsc.ts
@@ -1,5 +1,5 @@
-// import isEqual from 'lodash/isEqual';
-import { isEqual } from 'lodash';
+import isEqual from 'lodash/isEqual';
+// import { isEqual } from 'lodash';
 export function helloTsc(): string {
   console.log(isEqual('a', 'a'));
   return 'hello-tsc';
